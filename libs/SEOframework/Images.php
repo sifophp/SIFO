f8b108fe911d168de0d75f372c0694a3790623a1
Albert Lombarte <alombarte@gmail.com>
2011-08-07 18:22:39 +0200

    - Bootstrap: loads the debug when an Error exception is raised and the devel flag is enabled. - Image class adds an optional parameter to declare image quality definition. Defaults to 100%.
    Commit in behalf of Pablo Ros

--- libs/SEOframework/Images.php
+++ libs/SEOframework/Images.php
@@ -1,23 +1,4 @@
 <?php
-/**
- * LICENSE
- * 
- * Copyright 2010 Albert Garcia
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
 class Images
 {
 	/**
@@ -31,7 +12,7 @@ class Images
 	 * @param boolean $transparency
 	 * @return boolean
 	 */
-	static public function resizeAndSave( $from, $to, $width, $height, $crop = false, $resizeUp = false, $transparency = false )
+	static public function resizeAndSave( $from, $to, $width, $height, $crop = false, $resizeUp = false, $transparency = false, $quality = 100 )
 	{
 		include_once ROOT_PATH . '/libs/' . Config::getInstance()->getLibrary( 'phpthumb' ) . '/ThumbLib.inc.php';
 
@@ -41,7 +22,8 @@ class Images
 		$thumb = PhpThumbFactory::create( $from, array(
 					'resizeUp' => $resizeUp,
 					'preserveAlpha' => $transparency,
-					'preserveTransparency' => $transparency
+					'preserveTransparency' => $transparency,
+					'jpegQuality'	=> $quality,
 						) );
 		
 		if ( false === $crop )
