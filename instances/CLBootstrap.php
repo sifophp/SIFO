6d89edf1b316f6d70a1dc1853b5077d7f0d5aef8
Albert Lombarte <alombarte@gmail.com>
2012-02-03 22:28:43 +0100

    Corrected Command Line controller

--- instances/CLBootstrap.php
+++ instances/CLBootstrap.php
@@ -68,8 +68,8 @@ CLBootstrap::includeRequiredFiles();
 if ( !isset( $argv[1] ) || ( '-h' == $argv[1] ) || ( '--help' == $argv[1] ) )
 {
 	// Dump help info:
-	require_once ROOT_PATH . '/instances/common/controllers/shared/commandLineController.ctrl.php';
-	echo PHP_EOL . "Use 'php $argv[0] <domain> --help' for read the help info." . PHP_EOL . PHP_EOL;
+	require_once ROOT_PATH . '/instances/common/controllers/shared/commandLine.ctrl.php';
+	echo PHP_EOL . "Execute 'php $argv[0] <domain> --help' to read the help information." . PHP_EOL . PHP_EOL;
 	die;
 }
 CLBootstrap::$command_line_params = $argv;
