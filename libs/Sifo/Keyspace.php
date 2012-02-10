010ec2141d4ea69e2cec8c510338cc029731f01a
Albert Lombarte <alombarte@gmail.com>
2012-02-10 22:49:34 +0100

    Added Keyspace class, for better key management in Redis

---
+++ libs/Sifo/Keyspace.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Sifo;
+
+/**
+ * A Keyspace is a structured definition on how you will store your keys
+ * in a Key/Value storage system.
+ *
+ * You can use this class to access and set your Keys in systems like Redis.
+ * @requires A configuration file keyspace.config.php.
+ */
+class Keyspace
+{
+	/**
+	 * Contains all the keyspace mapping taken from configuration.
+	 *
+	 * @var array
+	 */
+	static protected $keyspace;
+
+	/**
+	 * Returns the final Key name for the given parameters.
+	 *
+	 * @param string $key_name
+	 * @param array $parameters
+	 * @throws KeySpace_Exception
+	 */
+	static public function get( $key_name, $parameters = null )
+	{
+		if ( !isset( self::$keyspace ) )
+		{
+			self::$keyspace = \Sifo\Config::getInstance()->getConfig( 'keyspace' );
+		}
+
+		if ( !isset( self::$keyspace[$key_name] ) )
+		{
+			throw new KeySpace_Exception( "Key named '$key_name' is not available in the key space." );
+		}
+
+
+		$key = self::$keyspace[$key_name];
+
+		if ( is_array( $parameters ) )
+		{
+			foreach ( $parameters as $tag => $value )
+			{
+				$tag = preg_quote( $tag, '/' );
+				$key = preg_replace( "/<($tag)>/", $value, $key);
+			}
+		}
+
+		// Remove any missing parameters:
+		if ( false !== strpos( $key, '<' ) )
+		{
+			throw new KeySpace_Exception( "The key contains undeclared parameters for replacement" );
+		}
+
+		return $key;
+	}
+
+}
+
+class KeySpace_Exception extends \Exception
+{
+
+}
--- libs/Smarty-sifo-plugins/function.pagelink.php
+++ libs/Smarty-sifo-plugins/function.pagelink.php
@@ -1,84 +1,58 @@
 <?php
 /**
  * Smarty plugin
+ *
  * @package Smarty
  * @subpackage plugins
  */
 
-
-/**
- * Smarty {fill} function plugin
- *
- * Type:     function<br>
- * Name:     fill<br>
- * Input:<br>
- *           - [any]      (required) - string
- *           - subject       (required) - string
- *           - delimiter  (optional, defaults to '%' ) - string
- * Purpose:  Fills the variables found in 'subject' with the paramaters passed. The variables are any word surrounded by two delimiters.
- *           
- *           Examples of usage:
- *           
- *           {fill subject="http://domain.com/profile/%username%" username='fred'}
- *           Output: http://domain.com/profile/fred
- *
- *           {fill subject="Hello %user%, welcome aboard!" user=Fred}
- *           Outputs: Hello Fred, welcome aboard
- *
- *           {fill subject="http://||subdomain||.domain.com/||page||/||action||" subdomain='www' page='my-first-post' action='vote' delimiter='||'}
- *           Outputs: http://www.domain.com/my-first-post/vote
- *
- * @link    http://www.harecoded.com/fill-smarty-php-plugin-311577
- * @author Albert Lombarte <alombarte at harecoded dot com>
- * @param array
- * @param Smarty
- * @return string
- */
-function smarty_function_pagelink($params, &$smarty)
+function smarty_function_pagelink( $params, &$smarty )
 {
 
-    if ( isset($params['delimiter']) )
-    {
-        $_delimiter = $params['delimiter'];
-        unset($params['delimiter']);
-    } else {
-        $_delimiter = ':';
-    }
-		$_actual_querystring = \Sifo\FilterServer::getInstance()->getString( 'QUERY_STRING' );
-		$_actual_path = \Sifo\FilterServer::getInstance()->getString( 'REQUEST_URI' );
-    
-    if ( !empty( $_actual_querystring ) )
-    {
-    	$_actual_querystring = '?' . $_actual_querystring;    
-    	$_actual_path = str_replace( $_actual_querystring,'',$_actual_path );
-	}
-
-	$_actual_url = array_reverse( explode( $_delimiter, $_actual_path ) );
-	
-	if ( is_numeric( $_actual_url[0] ) )
+	if ( isset( $params['delimiter'] ) )
 	{
-		$_actual_page = (int)array_shift( $_actual_url );
+		$_delimiter = $params['delimiter'];
+		unset( $params['delimiter'] );
 	}
 	else
 	{
-		$_actual_page = 1;
+		$_delimiter = ':';
 	}
-	
-	$_actual_url = implode( $_delimiter, array_reverse( $_actual_url ) );
-	
+	$_current_querystring = \Sifo\FilterServer::getInstance()->getString( 'QUERY_STRING' );
+	$_current_path = \Sifo\FilterServer::getInstance()->getString( 'REQUEST_URI' );
+
+	if ( !empty( $_current_querystring ) )
+	{
+		$_current_querystring = '?' . $_current_querystring;
+		$_current_path = str_replace( $_current_querystring, '', $_current_path );
+	}
+
+	$_current_url = array_reverse( explode( $_delimiter, $_current_path ) );
+
+	if ( is_numeric( $_current_url[0] ) )
+	{
+		$_current_page = ( int )array_shift( $_current_url );
+	}
+	else
+	{
+		$_current_page = 1;
+	}
+
+	$_current_url = implode( $_delimiter, array_reverse( $_current_url ) );
+
 	if ( !isset( $params['page'] ) )
 	{
-        trigger_error("pagelink: You should provide the destination pagelink.");  
+		trigger_error( "pagelink: You should provide the destination pagelink." );
 	}
 	else
 	{
 		if ( $params['page'] > 1 )
 		{
-			return $_actual_url . $_delimiter . $params['page'];
+			return $_current_url . $_delimiter . $params['page'];
 		}
 		else
 		{
-			return $_actual_url;
+			return $_current_url;
 		}
 	}
 }
