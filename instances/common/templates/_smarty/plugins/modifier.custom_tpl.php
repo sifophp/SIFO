829af9976b61a92b5ef7da49ed15f7dfadb5d4c5
Albert García <albert.garcia@obokaman.com>
2012-06-06 15:18:10 +0200

    Added custom_tpl Smarty plugin to common instance to allow all instances using extends and include Smarty functions taking into account instance inheritance.

---
+++ instances/common/templates/_smarty/plugins/modifier.custom_tpl.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Allow using {extends} and {include} Smarty functions taking into account the instance
+ * inheritance. Given template will be translated to full path tempalte file for
+ * active instance.
+ *
+ * @param $template
+ * @return string
+ */
+function smarty_modifier_custom_tpl( $template )
+{
+	if ( !isset( $template ) )
+	{
+		trigger_error( "custom_tpl: The attribute 'template' are not set", E_USER_NOTICE );
+	}
+
+	$instance_templates = \Sifo\Config::getInstance()->getConfig( 'templates' );
+	if ( isset( $instance_templates[$template] ) )
+	{
+		$selected_template = $instance_templates[$template];
+	}
+	else
+	{
+		trigger_error( "The template '{$template}' has not been found in the templates folder.", E_USER_ERROR );
+		return false;
+	}
+
+	return ( ROOT_PATH . "/$selected_template" );
+}
