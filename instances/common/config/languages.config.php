52b489a851d1b0d2b531e952216a7de437ebd0cf
Albert Lombarte <alombarte@gmail.com>
2012-10-30 16:21:25 +0100

    CSS Support fro Right To Left languages

--- instances/common/config/languages.config.php
+++ instances/common/config/languages.config.php
@@ -1,7 +1,7 @@
 <?php
 
 $config = array(
-			'ar_AE' => array( 'dir' => 'rtl', 'domain' => 'ar', 'name' => 'Arabic', 'translated' => 'عربي' ),
+			'ar_SA' => array( 'dir' => 'rtl', 'domain' => 'ar', 'name' => 'Arabic', 'translated' => 'عربي' ),
 			'ca_ES' => array( 'dir' => 'ltr', 'domain' => 'ca', 'name' => 'Catalan', 'translated' => 'Català' ),
 			'de_DE' => array( 'dir' => 'ltr', 'domain' => 'de', 'name' => 'German', 'translated' => 'Deutsch' ),
 			'en_US' => array( 'dir' => 'ltr', 'domain' => 'en', 'name' => 'English', 'translated' => 'English' ),
