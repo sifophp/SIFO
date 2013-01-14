42caf4227f64fb304d350c293f1040a95119fa7b
Toni <tonipinel@gmail.com>
2013-01-14 18:36:50 +0100

    Auto init modules for Core-Js

--- libs/Core-js/core/init.js
+++ libs/Core-js/core/init.js
@@ -15,30 +15,53 @@ CORE.behaviour.modules.isset = function( sId ) {
 
 
 $(document).ready(function() {
-	var CP = CORE.behaviour.page,
-		sId = '';
+	var sId = '',
+		oModules = document.querySelectorAll("[id]"),
+		sLoadedModules = 'LAUNCH MODULES: ';
+
+	// polyfill for querySelector
+	if (!document.querySelectorAll) {
+		document.querySelectorAll = function(selector) {
+	        var doc = document,
+	            head = doc.documentElement.firstChild,
+	            styleTag = doc.createElement('STYLE');
+	        head.appendChild(styleTag);
+	        doc.__qsaels = [];
+
+	        styleTag.styleSheet.cssText = selector + "{x:expression(document.__qsaels.push(this))}";
+	        window.scrollBy(0, 0);
+
+	        return doc.__qsaels;
+	    }
+	}
 
 	// To Execute behaviours based on modular elements
-	for ( var nCounter = 0; nCounter < CORE.modules.length; nCounter++  ) {
-		sId = CORE.modules[nCounter];
-		if ( document.getElementById( sId ) !== null )
+
+	for ( var nCounter = 0; nCounter < oModules.length; nCounter++  ) {
+		sId = oModules[nCounter].id;
+
+		if ( CORE.behaviour.modules[ sId ] !== undefined )
 		{
 			CORE.behaviour.modules[ sId ].init();
-			if ( console ) {
-				console.log( sId );
-			}
+			sLoadedModules += sId + ' | ';
 		}
 	}
 
-    // Always execute the common behaviour
-    CP['common']();
+	if ( console ) {
+		console.log( sLoadedModules + '\n' + 'TOTAL ID ELEMENTS:' + nCounter );
+	}
+
+	// Always execute the common behaviour (if exist)
+	if ( CORE.behaviour.common !== undefined ) {
+		CORE.behaviour.common();
+	}
 
 	// To execute behaviours based on pages
-	if (typeof CP[document.body.id] != "undefined")
+	if (typeof  CORE.behaviour.page[document.body.id] != "undefined")
 	{
         CP[document.body.id]();
 	} else {
-		CP.unset();
+		CORE.behaviour.page.unset();
 	}
 
 
