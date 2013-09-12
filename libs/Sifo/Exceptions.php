67caf31c91a2f5121d9785691d38a2e454fab06a
Albert Lombarte <alombarte@gmail.com>
2013-09-12 10:08:31 +0200

    500 errors will show a warning in the log file

--- libs/Sifo/Exceptions.php
+++ libs/Sifo/Exceptions.php
@@ -159,7 +159,14 @@ class Exception_405 extends SEO_Exception{}
 /**
  * Oooops. Internal server error.
  */
-class Exception_500 extends SEO_Exception{}
+class Exception_500 extends SEO_Exception{
+	public function __construct( $message = null, $code = 0 )
+	{
+		// Log 500 errors only:
+		trigger_error( '[ERROR 500]: ' . $message, E_USER_WARNING );
+		parent::__construct( $message, $code );
+	}
+}
 
 /**
  * Service unavailable.
