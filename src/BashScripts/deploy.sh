4dd31f48dcf9fd75c828ca11d7eb2d8043783c6a
Albert Lombarte <alombarte@gmail.com>
2014-10-06 21:49:03 +0200

    Deploy using composer

--- src/BashScripts/deploy.sh
+++ src/BashScripts/deploy.sh
@@ -4,28 +4,30 @@
 # This script updates the SIFO code to the latest and the passed instance as well.
 # It is meant to update your code in production.
 
+USER=`whoami`
+TODAY=$(date "+%Y-%b-%d")
+TIME=$(date "+%k:%M")
+
+
 ORIGINAL_PATH=$PWD
 SCRIPTPATH=$(cd ${0%/*} && echo $PWD/${0##*/})
-CORE=`dirname "$SCRIPTPATH"`
-CORE=`cd "${CORE}/../.." && pwd -P`
+APP_BASE_DIR=`dirname "$SCRIPTPATH"`
+APP_BASE_DIR=`cd "${APP_BASE_DIR}/../../../../.." && pwd -P`
 
 if [ $# != 1 ]
 then
     echo ""
     echo ""
-	echo "This script takes '$CORE' to HEAD revision of the current branches"
+	echo "This script puts '$APP_BASE_DIR' to its latest version in the current branch"
 	echo "--USAGE: $0 <instancename>"
 	echo ""
 	exit 0
 fi
 
 INSTANCE=$1
-LOG="/tmp/deploy_$INSTANCE.log"
-CHANGELOG="$CORE/instances/$INSTANCE/logs/deploy_LOG.txt"
-
-USER=`whoami`
-INSTANCEPATH="$CORE/instances/$INSTANCE"
-TODAY=`date "+%Y-%b-%d %k:%M"`
+INSTANCEPATH="$APP_BASE_DIR/instances/$INSTANCE"
+LOG_PATH="$INSTANCEPATH/logs/deploys"
+LOG="$LOG_PATH/deploy_$TODAY.log"
 
 
 #if [ "$USER" == "root" ]
@@ -35,67 +37,61 @@ TODAY=`date "+%Y-%b-%d %k:%M"`
 #        exit 0
 #fi
 
-mkdir -p $CORE/instances/$INSTANCE/logs/
+mkdir -p $APP_BASE_DIR/instances/$INSTANCE/logs/
+mkdir -p $LOG_PATH
 
-echo "Updating servers..." > $LOG
-echo "************************************************" >> $LOG
-echo "$INSTANCE update: $TODAY ($USER)" >> $LOG
-echo "************************************************" >> $LOG
+echo "" &&
+echo "***********************************************" &&
+echo "***************** D E P L O Y *****************" &&
+echo "***********************************************" &&
+echo "$INSTANCE update: $TODAY at $TIME ($USER)" &&
+echo "***********************************************" &&
+echo "" &&
+echo "Updating servers..." | tee -a $LOG
 
-cd $CORE
-SIFO_BRANCH=`git branch | awk '/^\*/ { print $2 }'`
-CORE_REMOTE_REV=`git ls-remote origin $SIFO_BRANCH | sed 's/\([0-9a-f]\{10\}\)\(.*\)/\1/g'`
-CORE_LOCAL_REV=`git rev-parse refs/heads/$SIFO_BRANCH | sed 's/\([0-9a-f]\{10\}\)\(.*\)/\1/g'`
+
+cd $APP_BASE_DIR
+APP_BRANCH=`git branch | awk '/^\*/ { print $2 }'`
+APP_BASE_DIR_REMOTE_REV=`git ls-remote origin $APP_BRANCH | sed 's/\([0-9a-f]\{10\}\)\(.*\)/\1/g' | head -n 1`
+APP_BASE_DIR_LOCAL_REV=`git rev-parse refs/heads/$APP_BRANCH | sed 's/\([0-9a-f]\{10\}\)\(.*\)/\1/g'`
 
 cd $INSTANCEPATH
 INSTANCE_BRANCH=`git branch | awk '/^\*/ { print $2 }'`
-INST_REMOTE_REV=`git ls-remote origin $INSTANCE_BRANCH | sed 's/\([0-9a-f]\{10\}\)\(.*\)/\1/g'`
+INST_REMOTE_REV=`git ls-remote origin $INSTANCE_BRANCH | sed 's/\([0-9a-f]\{10\}\)\(.*\)/\1/g' | head -n 1`
 INST_LOCAL_REV=`git rev-parse refs/heads/$INSTANCE_BRANCH | sed 's/\([0-9a-f]\{10\}\)\(.*\)/\1/g'`
 
-echo "---SIFO---"
-echo "---SIFO---" >> $LOG
-echo -e "From revision:\t$CORE_LOCAL_REV"
-echo -e "From revision:\t$CORE_LOCAL_REV" >> $LOG
-echo -e "To revision:\t$CORE_REMOTE_REV"
-echo -e "To revision:\t$CORE_REMOTE_REV" >> $LOG
-echo -e "BRANCH: $SIFO_BRANCH"
-echo -e "BRANCH: $SIFO_BRANCH" >> $LOG
-echo "---$INSTANCE---"
-echo "---$INSTANCE---" >> $LOG
-echo -e "From revision:\t$INST_LOCAL_REV"
-echo -e "From revision:\t$INST_LOCAL_REV" >> $LOG
-echo -e "To revision:\t$INST_REMOTE_REV"
-echo -e "To revision:\t$INST_REMOTE_REV" >> $LOG
-echo -e "BRANCH: $INSTANCE_BRANCH"
-echo -e "BRANCH: $INSTANCE_BRANCH" >> $LOG
-echo "************************************************"
-echo "************************************************" >> $LOG
+echo "---APP---" &&
+echo -e "From revision:\t$APP_BASE_DIR_LOCAL_REV" &&
+echo -e "To revision:\t$APP_BASE_DIR_REMOTE_REV"  &&
+echo "BRANCH: $APP_BRANCH" &&
+echo "---$INSTANCE---" &&
+echo -e "From revision:\t$INST_LOCAL_REV" &&
+echo -e "To revision:\t$INST_REMOTE_REV" &&
+echo "BRANCH: $INSTANCE_BRANCH" &&
+echo "************************************************" | tee -a $LOG
 
-if [ "$CORE_LOCAL_REV" == "$CORE_REMOTE_REV" ]
+if [ "$APP_BASE_DIR_LOCAL_REV" == "$APP_BASE_DIR_REMOTE_REV" ]
 then
-    echo -e "SIFO is already in the latest revision: $CORE_LOCAL_REV"
-    echo -e "SIFO is already in the latest revision: $CORE_LOCAL_REV" >> $LOG
+    echo "Your app is already in the latest revision: $APP_BASE_DIR_LOCAL_REV" | tee -a $LOG
 else
-    cd $CORE
-    git pull origin $SIFO_BRANCH >> $LOG
-    # echo "What changed in SIFO..." >> $LOG
-    # git whatchanged $CORE_LOCAL_REV..$CORE_REMOTE_REV >> $LOG
+    cd $APP_BASE_DIR
+    git pull origin $APP_BRANCH 2>&1 | tee -a $LOG
+    # echo "What changed in SIFO..." | tee -a $LOG
+    # git whatchanged $APP_BASE_DIR_LOCAL_REV..$APP_BASE_DIR_REMOTE_REV | tee -a $LOG
 fi
 
 if [ "$INST_REMOTE_REV" == "$INST_LOCAL_REV" ]
 then
-    echo -e "Instance $INSTANCE is already in the latest revision: $INST_REMOTE_REV"
-    echo -e "Instance $INSTANCE is already in the latest revision: $INST_REMOTE_REV" >> $LOG
+    echo "Instance $INSTANCE is already in the latest revision: $INST_REMOTE_REV" | tee -a $LOG
 else
     cd $INSTANCEPATH
-    git pull origin $INSTANCE_BRANCH >> $LOG
-    echo "What changed in $INSTANCE..." >> $LOG
-    git whatchanged $INST_LOCAL_REV..$INST_REMOTE_REV >> $LOG
+    git pull origin $INSTANCE_BRANCH | tee -a $LOG
+    echo "What changed in $INSTANCE..." | tee -a $LOG
+    git whatchanged $INST_LOCAL_REV..$INST_REMOTE_REV | tee -a $LOG
 fi
+
+echo "Installing your composer.lock dependencies" | tee -a $LOG
+composer install | tee -a $LOG
 
 # Leaver user in the original path before executing this script
 cd $ORIGINAL_PATH
-
-# Append current log to accumulative changelog
-cat $LOG >> $CHANGELOG
-cat $LOG
