a850cbcb4544a5c46927017be7611ff1d1bd8ccd
Albert Lombarte <alombarte@gmail.com>
2014-10-13 16:53:52 +0200

    Fixed Command Line controller paths

--- src/Sifo/CLBootstrap.php
+++ src/Sifo/CLBootstrap.php
@@ -109,7 +109,8 @@ CLBootstrap::includeRequiredFiles();
 if ( !isset( $argv[1] ) || ( '-h' == $argv[1] ) || ( '--help' == $argv[1] ) )
 {
 	// Dump help info:
-	require_once ROOT_PATH . '/instances/common/controllers/shared/commandLine.ctrl.php';
+	require_once ROOT_PATH . '/vendor/sifophp/sifo-common-instance/controllers/shared/commandLine.php';
+
 	echo PHP_EOL . "Execute 'php $argv[0] <domain> --help' to read the help information." . PHP_EOL . PHP_EOL;
 	echo "Your available domains:" . PHP_EOL;
 	$available_domains = CLBootstrap::get_available_domains();
