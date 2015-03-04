34db1e19fbf03a0850154ccf9821dccf87318e83
Albert Garcia <albert.garcia@uvinum.com>
2015-03-04 10:31:09 +0100

    Remove trigger_error from Exception_500

--- src/Sifo/Exceptions.php
+++ src/Sifo/Exceptions.php
@@ -159,14 +159,7 @@ class Exception_405 extends SEO_Exception{}
 /**
  * Oooops. Internal server error.
  */
-class Exception_500 extends SEO_Exception{
-	public function __construct( $message = null, $code = 0 )
-	{
-		// Log 500 errors only:
-		trigger_error( '[ERROR 500]: ' . $message, E_USER_WARNING );
-		parent::__construct( $message, $code );
-	}
-}
+class Exception_500 extends SEO_Exception{}
 
 /**
  * Service unavailable.
