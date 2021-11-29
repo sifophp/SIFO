a7422af426c78740c1553d07b2fc67e2a1b424ca
Sergi Ullastre <workwithseergiue@gmail.com>
2021-11-29 10:48:20 +0100

    MDI-1441 Removed/replaced deprecated unset($this)

--- src/adodb5/adodb-xmlschema.inc.php
+++ src/adodb5/adodb-xmlschema.inc.php
@@ -156,8 +156,8 @@ class dbObject {
 	/**
 	* Destroys the object
 	*/
-	function destroy() {
-		unset( $this );
+	static function destroy(&$obj) {
+		$obj = null;
 	}
 	
 	/**
@@ -347,7 +347,7 @@ class dbTable extends dbObject {
 			case 'TABLE':
 				$this->parent->addSQL( $this->create( $this->parent ) );
 				xml_set_object( $parser, $this->parent );
-				$this->destroy();
+				self::destroy($this);
 				break;
 			case 'FIELD':
 				unset($this->current_field);
@@ -1064,7 +1064,7 @@ class dbQuerySet extends dbObject {
 			case 'SQL':
 				$this->parent->addSQL( $this->create( $this->parent ) );
 				xml_set_object( $parser, $this->parent );
-				$this->destroy();
+				self::destroy($this);
 				break;
 			default:
 				
@@ -2175,16 +2175,6 @@ class adoSchema {
 		}
 		
 		return $this->sqlArray;
-	}
-	
-	/**
-	* Destroys an adoSchema object.
-	*
-	* Call this method to clean up after an adoSchema object that is no longer in use.
-	* @deprecated adoSchema now cleans up automatically.
-	*/
-	function Destroy() {
-		unset( $this );
 	}
 }
 
